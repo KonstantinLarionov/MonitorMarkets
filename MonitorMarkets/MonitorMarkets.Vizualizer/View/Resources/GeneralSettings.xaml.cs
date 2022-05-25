1c70ddbf81cf4b45cd9c875bee135dfc93c71e48
Konstantin Larionov <kostya12277@yandex.ru>
2022-05-25 10:48:48 +0500

    Create GeneralSettings

---
+++ MonitorMarkets/MonitorMarkets.Vizualizer/View/Resources/GeneralSettings.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using MaterialDesignThemes.Wpf;
+
+namespace MonitorMarkets.Vizualizer.View.Resources
+{
+    public partial class GeneralSettings : Window
+    {
+        public GeneralSettings()
+        {
+            InitializeComponent();
+        }
+
+        public void ComboBox()
+        {
+        }
+    }
+}
