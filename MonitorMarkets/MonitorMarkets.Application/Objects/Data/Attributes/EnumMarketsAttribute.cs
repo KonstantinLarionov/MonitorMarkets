728d2eadc53e83ee809d57132ee8cd06f8703bf4
Константин Ларионов <larionov@scancity.ru>
2022-05-25 23:02:42 +0500

    create shem with BinanceClient

---
+++ MonitorMarkets/MonitorMarkets.Application/Objects/Data/Attributes/EnumMarketsAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MonitorMarkets.Application.Objects.Data.Attributes
+{
+    public class EnumMarketsAttribute : Attribute
+    {
+        public EnumMarketsAttribute(string nameMarket, string apiKey, string secretKey, string passPhrase, bool isActive)
+        {
+            NameMarket = nameMarket;
+            ApiKey = apiKey;
+            SecretKey = secretKey;
+            PassPhrase = passPhrase;
+            IsActive = isActive;
+        }
+
+        public string NameMarket { get; set; }
+        public string ApiKey { get; set; }
+        public string SecretKey { get; set; }
+        public string PassPhrase { get; set; }
+        public bool IsActive { get; }
+        public bool isActive { get; set; }
+    }
+}
