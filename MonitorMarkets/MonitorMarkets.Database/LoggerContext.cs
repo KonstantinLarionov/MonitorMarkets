4e87b2c9a46166eb86e900fb2b5fd52996effa66
Аитов Карим <aitovkarim@yandex.ru>
2022-05-30 11:32:03 +0500

    fix database

--- MonitorMarkets/MonitorMarkets.Database/LoggerContext.cs
+++ MonitorMarkets/MonitorMarkets.Database/LoggerContext.cs
@@ -1,22 +1,14 @@
 ﻿using System.Data.Entity;
 using MonitorMarkets.Database.Entities;
-using Microsoft.Data.Sqlite;
-using Microsoft.EntityFrameworkCore; 
 
 namespace MonitorMarkets.Database
 {
     public class LoggerContext : DbContext
     {
+        public LoggerContext() :base("DefaultConnection")
+        {}
         
         public DbSet<Log> Users { get; set; }
-        
-        
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            var connectionStringBuilder = new SqliteConnectionStringBuilder { DataSource = $"MonitorDB.db" };
-            var connectionString = connectionStringBuilder.ToString();
-            var connection = new SqliteConnection(connectionString);
-            optionsBuilder.UseSqlite(connection);
-        }
+         
     }
 }
--- MonitorMarkets/MonitorMarkets.Database/LoggerRepository.cs
+++ MonitorMarkets/MonitorMarkets.Database/LoggerRepository.cs
@@ -1,7 +1,26 @@
-﻿namespace MonitorMarkets.Database
+﻿using System.Data.Entity;
+using MonitorMarkets.Database.Entities;
+
+namespace MonitorMarkets.Database
 {
     public class LoggerRepository
     {
-            
+        LoggerContext _context;
+        DbSet<Log> _dbSet;
+        public void Create(Log item)
+        {
+            _dbSet.Add(item);
+            _context.SaveChanges();
+        }
+        public void Update(Log item)
+        {
+            _context.Entry(item).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+        public void Remove(Log item)
+        {
+            _dbSet.Remove(item);
+            _context.SaveChanges();
+        }
     }
 }
