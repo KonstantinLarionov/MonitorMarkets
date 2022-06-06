7e7159afe220af481fa5aba754390b5c4e222b9f
Nikita Panin <nik-panin-2016@mail.ru>
2022-06-06 17:04:08 +0500

    Begin RestSharp commands

--- MonitorMarkets/MonitorMarkets.Vizualizer/View/GeneralSettingsWindow.xaml.cs
+++ MonitorMarkets/MonitorMarkets.Vizualizer/View/GeneralSettingsWindow.xaml.cs
@@ -1,23 +1,76 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Data.Entity.Infrastructure;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
 using System.Windows;
+using RestSharp;
 using MonitorMarkets.Application.Objects.Abstractions;
 using MonitorMarkets.Contexts;
 using MonitorMarkets.Contexts.Entities;
+using RestSharp.Authenticators;
+using Swagger.Net.Annotations;
 
 namespace MonitorMarkets.Vizualizer.View
 {
+    
+    protected async ValueTask<Parameter> GetAuthenticationParameter(string accessToken) {
+        var token = string.IsNullOrEmpty(Token) ? await GetToken() : Token;
+        return new HeaderParameter(KnownHeaders.Authorization, token);
+    }
+    async Task<string> GetToken() {
+        var options = new RestClientOptions("https://localhost:7116");
+        using var client = new RestClient(options) {
+            Authenticator = new HttpBasicAuthenticator(root, root),
+        };
+
+        var request = new RestRequest("oauth2/token")
+            .AddParameter("grant_type", "client_credentials");
+        var response = await client.PostAsync<TokenResponse>(request);
+        return quot;{response!.TokenType} {response!.AccessToken}";
+    }
     public partial class GeneralSettingsWindow : Window
     {
-        // private readonly IRepository<ConnectionKeys>
-        public GeneralSettingsWindow()
+        readonly string _baseUrl;
+        readonly string _clientId;
+        readonly string _clientSecret;
+        
+       
+        public GeneralSettingsWindow(string baseUrl, string clientId, string clientSecret)
         {
-            /*DesktopContext db = new DesktopContext();
-            db.ConnectionsKeys.Add(new ConnectionsKeys()
-                { DateTime = DateTime.Now, PassPhrase = "123", PublicKeys = "321", SecretKey = "54353" });
-            db.SaveChanges();
-            */
-            
+            _baseUrl = baseUrl;
+            _clientId = clientId;
+            _clientSecret = clientSecret;
             InitializeComponent();
+            GetKeys();
+        }
+        
+
+        public void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            string publicKey = PublicKeyTextBox.Text;
+            string secretKey= PublicKeyTextBox.Text;
+            string passPhrase = PublicKeyTextBox.Text;
+            string marketsEnum = PublicKeyTextBox.Text;
+            ChangeKeys(publicKey, secretKey, passPhrase, marketsEnum);
+        }
+        public void ChangeKeys(string publicKey,string secretKey, string passPhrase, string marketsEnum)
+        {
+            var request = new RestRequest("https://localhost:7116/UpdateKey");
+            request.AddOrUpdateParameter("PublicKey", publicKey);
+            request.AddOrUpdateParameter("SecretKey", secretKey );
+            request.AddOrUpdateParameter("PassPhrase", passPhrase);
+            request.AddOrUpdateParameter("MarketEnums",marketsEnum);
+        }
+
+        public void GetKeys()
+        {
+            var request = new RestRequest("https://localhost:7116/GetKey");
+            var response = request.Method == Method.GET;
         }
     }
+    
 }
